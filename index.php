b56eba47a0e98ed7a37f1f1146740ede4aeb07a3
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-08-22 16:20:14 +0200

    test

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<?php 
-    // Variabile
-    $prohibitedWords = 'Zeus (in greco antico: Ζεύς, Zèus) nella religione greca è il re di tutti gli dèi, il capo dell Olimpo, dio del tuono. I suoi simboli sono la folgore, il toro, l aquila, la quercia e l olivo.
+<?php
+// Variabile
+$prohibitedWords = 'Zeus (in greco antico: Ζεύς, Zèus) nella religione greca è il re di tutti gli dèi, il capo dell Olimpo, dio del tuono. I suoi simboli sono la folgore, il toro, l aquila, la quercia e l olivo.
 
     Figlio del titano Crono e di Rea, è il più giovane dei suoi fratelli e sorelle: Estia, Demetra, Era, Ade e Poseidone. Nella maggior parte delle leggende o miti é sposato con Era (protettrice del matrimonio e dei figli), anche se nel santuario dell oracolo di Dodona come sua consorte si venerava Dione (viene raccontato nell Iliade che Zeus sia il padre di Afrodite, avuta con Dione).
     
@@ -11,22 +11,46 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Badwords Php</title>
 </head>
+
 <body>
     <h1>Badwords Php</h1>
-    <p>
-        <?php
-            var_dump($prohibitedWords);
+    
+    <div>
 
-            var_dump(str_replace('Zeus', '***', $prohibitedWords));
+        <p>
+            <?php
+                echo $prohibitedWords;
+            ?>
+        </p>
 
-            echo str_replace('Zeus', '***', $prohibitedWords);
-        ?>
-    </p>
+        <pre>
+            <?php
+                echo ' length ' . strlen($prohibitedWords) . ' words';
+            ?>
+        </pre>
+    </div>
+
+    <div>
+
+        <p>
+            <?php
+                echo str_replace('Zeus', '***', $prohibitedWords);
+            ?>
+        </p>
+
+        <pre>
+            <?php
+                echo ' length ' . strlen($prohibitedWords) . ' words';
+            ?>
+        </pre>
+    </div>
 </body>
+
 </html>
