796afb90df7ca0531ac91f07a006b00467db6f41
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-08-22 13:23:07 +0200

    show paragraph and lenght

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+    // Variabile
+    $proibitedWords = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti possimus soluta, consequatur minima animi facere? Modi odit distinctio ipsa veritatis dicta, aperiam dolorem ducimus voluptas non. Laboriosam amet neque incidunt?';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Badwords Php</title>
+</head>
+<body>
+    <h1>Badwords Php</h1>
+    <p>
+        <?php
+            var_dump($proibitedWords);
+        ?>
+    </p>
+</body>
+</html>
